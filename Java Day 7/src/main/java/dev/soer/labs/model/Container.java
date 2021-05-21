20e5ac49df594ce343a19e006b49db758231008e
Joseph Soer <soer.joseph@gmail.com>
2021-05-20 20:29:05 -0500

    Day 7 Labs and Notes

---
+++ Java Day 7/src/main/java/dev/soer/labs/model/Container.java
@@ -0,0 +1,20 @@
+package dev.soer.labs.model;
+
+public class Container<T> {
+
+	private T obj;
+	
+	public void setObject(T obj) {
+		this.obj = obj;
+	}
+	
+	public T getObject() {
+		return this.obj;
+	}
+	
+	public T removeObject() {
+		T temp = this.obj;
+		obj = null;
+		return temp;
+	}
+}
